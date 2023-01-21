9a047e7ff835e8f3c1e75990d0beb3425d641129
albina2604 <albinamilshyna2604@gmail.com>
2023-01-21 13:55:31 +0200

    Add update and draw_bullet

--- bullet.py
+++ bullet.py
@@ -19,3 +19,12 @@ class Bullet(Sprite):
 
         # Позиция снаряда хранится в вещественном формате
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Перемещает снаряд вверх по экрану"""
+        self.y -= self.settings.bullet_speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Вывод снаряда на экран"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
