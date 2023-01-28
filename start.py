a81b50fb2878de08a1270007a55d2aaf8b24393a
albina2604 <albinamilshyna2604@gmail.com>
2023-01-28 14:39:32 +0200

    Add alien rows

--- start.py
+++ start.py
@@ -33,20 +33,28 @@ class AlienInvasion:
         # Создание пришельца и расчет количества пришельцев в ряду
         # Интервал между соседними пришельцами равен ширине пришельца
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Создание первого ряда пришельцев
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Определение количества рядов
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        # Создание флота пришельцев
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         # Создание пришельца и расположение его в ряду
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
     def run_game(self):
