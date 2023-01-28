d95145eb0ac79accd20b6e3589f63d46796cea4e
albina2604 <albinamilshyna2604@gmail.com>
2023-01-28 15:31:44 +0200

    Add fleet movement

--- start.py
+++ start.py
@@ -57,6 +57,19 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self.change_fleet_direction()
+                break
+
+    def change_fleet_direction(self):
+        """Опускает весь флот и изменяет направление движения"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def run_game(self):
         """Запуск основного центра игры"""
         while True:
@@ -111,6 +124,7 @@ class AlienInvasion:
 
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев с флота"""
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_screen(self):
