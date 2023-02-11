c6eea8eca7f44867674fca7b18c3a1a94b43af83
albina2604 <albinamilshyna2604@gmail.com>
2023-02-11 14:09:04 +0200

    Update button Play

--- start.py
+++ start.py
@@ -100,6 +100,14 @@ class AlienInvasion:
                 self._check_keydown_event(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Запускает новую игру после нажатия кнопки Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _check_keydown_event(self, event):
         """Реагирует на нажатие клавиш"""
